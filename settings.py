010c1a42787e05794827e1af7520f85bbbf795bb
kumaratinfy <kumar.garg@gmail.com>
2016-01-03 21:24:45 +0530

    Completed user authentication and initial setup

--- settings.py
+++ settings.py
@@ -12,5 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '132426277670-61mrfagg3ca04d1mbmhp0717p3rbnbc4.apps.googleusercontent.com'
 
--- static/js/app.js
+++ static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '132426277670-61mrfagg3ca04d1mbmhp0717p3rbnbc4.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
